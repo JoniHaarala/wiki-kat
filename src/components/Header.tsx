3464e6aff326f468c8a83ed83ea20b53c41b87e7
jonatan <jonatan.haaralaorosco@gmail.com>
2023-09-10 22:37:52 -0300

    link tu root page added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import logo from '@/assets/CatwikiLogo.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function Header() {
     return (
         <header className='w-full py-5 max-w-6xl'>
-            <Image src={logo} alt='logo' />
+            <Link href={'/'}>
+                <Image src={logo} alt='logo' />
+            </Link>
+
         </header>
     )
 }
